cab5dd9f5f60dbf462467ae14e62f873ab185fb2
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-15 21:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	char=' '
+  let res=''
+  let space=s.indexOf(char)
+  if(space>0){
+  for(i=0;i<space;i++){
+      res+=s[i]
+  }}else{
+      return s
+  }
+  return res
+	
 }
 
 // Do not change the code below
